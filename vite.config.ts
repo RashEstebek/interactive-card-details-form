ba30cca42d0fa8ba9692d2608f27cba67e69a82e
Rash Estebek <98695442+RashEstebek@users.noreply.github.com>
2024-01-12 20:41:12 +0600

    config: update vite configuration file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://RashEstebek.github.io/interactive-card-details-form",
   plugins: [svelte()],
 })
